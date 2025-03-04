0c06ee9de0976cc6c2f3a68e1084645a1babca00
ismaelyoussef1009 <soumayabya73@gmail.com>
2025-03-04 16:37:14 +0100

    3 commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PostModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
